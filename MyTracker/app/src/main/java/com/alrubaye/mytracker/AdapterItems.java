7d2669451968b07e7742ec1f28eb46e6f78b444b
Akshat jain <akshatjain.88008@gmail.com>
2018-08-28 21:56:50 +0530

    RoadSafety
    we have make a some code for Road safety app
    which includes location,maps,sms and etc

---
+++ MyTracker/app/src/main/java/com/alrubaye/mytracker/AdapterItems.java
@@ -0,0 +1,15 @@
+package com.alrubaye.mytracker;
+
+
+
+public class AdapterItems
+{
+
+    public  String UserName;
+    public  String PhoneNumber;
+    AdapterItems(  String UserName,String PhoneNumber)
+    {
+        this. UserName=UserName;
+        this. PhoneNumber=PhoneNumber;
+    }
+}
